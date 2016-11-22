27c745089f19862dc2f5a00d4b722b39b8b63761
KevCoffee <kevinzhouusa@gmail.com>
2016-11-21 19:19:21 -0600

    Gyro Sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroSensorTest.java
@@ -0,0 +1,138 @@
+/* Copyright (c) 2014, 2015 Qualcomm Technologies Inc
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Demonstrates empty OpMode
+ */
+//@Autonomous(name = "Test: Motor Encoder", group = "Concept")
+@TeleOp(name = "Test: Gyro Sensor Test", group = "Linear Opmode")
+public class GyroSensorTest extends OpMode {
+
+  private ElapsedTime runtime = new ElapsedTime();
+  private ElapsedTime speedTimer = new ElapsedTime();
+
+  //Initialize Variables
+  DcMotor leftMotor = null;
+  DcMotor rightMotor = null;
+  //ColorSensor robotColorSensor = null;
+  //ColorSensor otherColorSensor = null;
+  GyroSensor robotGyroSensor = null;
+  //All units here is inches
+  private final int ticksPerRotation = 1120;
+  private int motorTarget = 3 * ticksPerRotation;
+  private int realTimeTicks = 0;
+
+  private double time = 0;
+  private double wheelDiameter = 2.75;
+
+  private double olderTick = 0;
+  private double currentTick = 0;
+  private double lastSecondsTick = 0;
+  private double motorSpeed = 0;
+
+  private boolean LEDStatus = false;
+
+
+  @Override
+  public void init() {
+    telemetry.addData("Status", "Initialized");
+    //robotColorSensor = hardwareMap.colorSensor.get("color sensor 1");
+    //robotColorSensor.enableLed(false);
+    //otherColorSensor = hardwareMap.colorSensor.get("color sensor 2");
+    robotGyroSensor = hardwareMap.gyroSensor.get("gyro sensor");
+    //otherColorSensor.enableLed(false);
+    robotGyroSensor.calibrate();
+  }
+
+  /*
+     * Code to run when the op mode is first enabled goes here
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+     */
+  @Override
+  public void init_loop() {
+
+   // robotColorSensor.enableLed(LEDStatus);
+    //otherColorSensor.enableLed(LEDStatus);
+
+  }
+
+  /*
+   * This method will be called ONCE when start is pressed
+   * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+   */
+  @Override
+  public void start() {
+    //Test for color sensor
+    //robotColorSensor.enableLed(LEDStatus);
+    //otherColorSensor.enableLed(LEDStatus);
+
+    robotGyroSensor.calibrate();
+    telemetry.addData("Is calibrating?", robotGyroSensor.isCalibrating());
+  }
+
+  /*
+   * This method will be called repeatedly in a loop
+   * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+   *///
+  //The code for the robot while running driver control
+  //This method acts as a while loop
+  @Override
+  public void loop() {
+    //Driver Controller
+    //robotColorSensor.enableLed(LEDStatus);
+    //otherColorSensor.enableLed(LEDStatus);
+
+    //robotColorSensor.setI2cAddress(I2cAddr)
+
+    /*telemetry.addData("Sensor placement 1: ", robotColorSensor.getI2cAddress());
+    //telemetry.addData("Sensor placement 2: ", otherColorSensor.getI2cAddress());
+
+    telemetry.addData("Color sensor blue: ", robotColorSensor.blue());
+    telemetry.addData("Color sensor red: ", robotColorSensor.red());
+    telemetry.addData("Color sensor hue: ", robotColorSensor.argb());*/
+
+    //telemetry.addData("Other sensor blue: ", otherColorSensor.blue());
+    //telemetry.addData("Other sensor red: ", otherColorSensor.red());
+
+    //telemetry.addData("LED Status: ", LEDStatus);
+    telemetry.addData("Gyro X Value", robotGyroSensor.rawX());
+    telemetry.addData("Gyro Y Value", robotGyroSensor.rawY());
+    telemetry.addData("Gyro Z Value", robotGyroSensor.rawZ());
+    telemetry.addData("Heading!", robotGyroSensor.getHeading());
+    telemetry.addData("Sensor Status: ", robotGyroSensor.status());
+
+
+  }
+}
