ab232a2788582e2c113854ce10d07751c1285366
lm-sb1 <luke@marshallvillage.com>
2017-01-05 19:40:05 -0600

    not really anything

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous3rdEdition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous3rdEdition.java
@@ -245,22 +245,11 @@ public class Autonomous3rdEdition extends LinearOpMode {
         haltDrive();
     }
     public void turnLeftVariableAndStop(double power, int degrees) {
-
-          /*while (true/*absHeading() < (heading - threshold) || absHeading() > heading + threshold) {
-              robot.frontLeftMotor.setPower(power);
-              robot.backLeftMotor.setPower(power);
-              robot.frontRightMotor.setPower(-power);
-              robot.backRightMotor.setPower(-power);
-          }
-          robot.frontLeftMotor.setPower(0);
-          robot.backLeftMotor.setPower(0);
-          robot.frontRightMotor.setPower(0);
-          robot.backRightMotor.setPower(0);*/
         calcTurn(degrees);
-        //robot.frontRightMotor.setTargetPosition(robot.frontRightMotor.getCurrentPosition() - counts);
+
         robot.backLeftMotor.setTargetPosition(robot.backLeftMotor.getCurrentPosition() + counts);
         robot.backRightMotor.setTargetPosition(robot.backRightMotor.getCurrentPosition() - counts);
-        //robot.frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         robot.backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         while(!((robot.backLeftMotor.getCurrentPosition()<= robot.backLeftMotor.getTargetPosition()-TOLERANCE) &&
