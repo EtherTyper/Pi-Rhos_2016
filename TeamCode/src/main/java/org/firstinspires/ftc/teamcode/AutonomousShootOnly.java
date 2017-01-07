a2ead37b81aaef5b649514e59ffe8dc446996747
lm-sb1 <luke@marshallvillage.com>
2017-01-06 22:21:01 -0600

    add another Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousShootOnly.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by lukemarshall on 1/6/17.
+ */
+        import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.util.ElapsedTime;
+@Autonomous(name = "Autonomous Shoot Only Red", group = "Concept")
+public class AutonomousShootOnly {
+
+}
